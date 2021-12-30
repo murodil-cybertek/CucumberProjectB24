2fbc1a7e9a083984cdba83f9f02952d25c1e3a21
Murodil <murodil@cybertekschool.com>
2021-12-29 20:33:37 -0500

    DAY 04 - CALCULATOR, BACKGROUND

---
+++ src/test/java/com/cybertek/step_definitions/CalculatorStepDefs.java
@@ -0,0 +1,22 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CalculatorStepDefs {
+
+    @Given("User is on calculator page")
+    public void user_is_on_calculator_page() {
+
+    }
+
+    @Given("User clicks on {string} on calculator")
+    public void user_clicks_on_on_calculator(String string) {
+
+    }
+
+    @Then("result {string} should be displayed")
+    public void result_should_be_displayed(String string) {
+
+    }
+}
