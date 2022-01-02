29468cffb027a9a4e60d55d22c86862794615a1e
Murodil <murodil@cybertekschool.com>
2022-01-02 15:09:46 -0500

    DAY 06 - rerun failed tests

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" //store failed scenario names into rerun.txt file
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
