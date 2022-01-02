23c6176781e8c740129240966bba5589c384f1b9
Murodil <murodil@cybertekschool.com>
2022-01-02 12:27:51 -0500

    DAY 06 - scenario outline practice

--- src/test/java/com/cybertek/step_definitions/CloudTablesStepDefs.java
+++ src/test/java/com/cybertek/step_definitions/CloudTablesStepDefs.java
@@ -1,5 +1,6 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.CloudTablesPage;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -7,6 +8,9 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class CloudTablesStepDefs {
+
+    CloudTablesPage cloudTablesPage=new CloudTablesPage();
+
     @Given("User is on cloudtables homepage")
     public void user_is_on_cloudtables_homepage() {
         Driver.getDriver().get(ConfigurationReader.getProperty("cloudtables.url"));
@@ -15,31 +19,32 @@ public class CloudTablesStepDefs {
 
     @When("User clicks on New button")
     public void user_clicks_on_new_button() {
-
+        cloudTablesPage.newBtn.click();
     }
 
     @When("User enters {string} to firstname field")
-    public void user_enters_to_firstname_field(String string) {
-
+    public void user_enters_to_firstname_field(String firstName) {
+        cloudTablesPage.firstNameField.sendKeys(firstName);
+        //cloudTablesPage.enterFirstName(firstName);
     }
 
     @When("User enters {string} to lastname field")
-    public void user_enters_to_lastname_field(String string) {
-
+    public void user_enters_to_lastname_field(String lastName) {
+        cloudTablesPage.lastNameField.sendKeys(lastName);
     }
 
     @When("User enters {string} to position field")
-    public void user_enters_to_position_field(String string) {
-
+    public void user_enters_to_position_field(String position) {
+        cloudTablesPage.positionField.sendKeys(position);
     }
 
     @When("User enters {string} to salary field")
-    public void user_enters_to_salary_field(String string) {
-
+    public void user_enters_to_salary_field(String salary) {
+        cloudTablesPage.salaryField.sendKeys(salary);
     }
 
     @When("User clicks on create button")
     public void user_clicks_on_create_button() {
-
+        cloudTablesPage.createBtn.click();
     }
 }
