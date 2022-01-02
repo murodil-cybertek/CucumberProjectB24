a89265ca0aa3b40d424bd3cc73f004156b6734d5
Murodil <murodil@cybertekschool.com>
2022-01-02 11:25:55 -0500

    DAY 06 - scenario outline practice

---
+++ src/test/java/com/cybertek/step_definitions/CloudTablesStepDefs.java
@@ -0,0 +1,41 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class CloudTablesStepDefs {
+    @Given("User is on cloudtables homepage")
+    public void user_is_on_cloudtables_homepage() {
+
+    }
+
+    @When("User clicks on New button")
+    public void user_clicks_on_new_button() {
+
+    }
+
+    @When("User enters {string} to firstname field")
+    public void user_enters_to_firstname_field(String string) {
+
+    }
+
+    @When("User enters {string} to lastname field")
+    public void user_enters_to_lastname_field(String string) {
+
+    }
+
+    @When("User enters {string} to position field")
+    public void user_enters_to_position_field(String string) {
+
+    }
+
+    @When("User enters {string} to salary field")
+    public void user_enters_to_salary_field(String string) {
+
+    }
+
+    @When("User clicks on create button")
+    public void user_clicks_on_create_button() {
+
+    }
+}
