b8571db91388099b3cc12c401f1d7fd9745d8c2f
Murodil <murodil@cybertekschool.com>
2022-01-02 15:44:58 -0500

    DAY 06 - rerun failed tests

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
-        dryRun = false, //false=> run the test. true => check for missing steps
-        tags = "@cloudtables"
+        dryRun = true, //false=> run the test. true => check for missing steps
+        tags = "@rerun"
 )
 public class CukesRunner {
 }
