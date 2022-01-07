5d8c00de88e4545cd8c7a17fd62368455ecbee67
Murodil <murodil@cybertekschool.com>
2022-01-06 21:00:46 -0500

    DAY 08 - excel automation day 2

--- src/test/java/com/cybertek/tests/ExcelWrite.java
+++ src/test/java/com/cybertek/tests/ExcelWrite.java
@@ -1,9 +1,12 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ExcelWrite {
@@ -13,12 +16,24 @@ public class ExcelWrite {
     @Test
     public void writeToExcelSheet() throws IOException {
         //open the Employees.xlsx using Apache POI
-        XSSFWorkbook workbook = new XSSFWorkbook(filePath);
+        FileInputStream in = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(in);
 
         //Goto "data" sheet. or goto first sheet by index
         XSSFSheet dataSheet = workbook.getSheetAt(0);
 
+        //add a column name "Salary" and save into file
+        XSSFCell salaryColCell = dataSheet.getRow(0).createCell(3);
+        salaryColCell.setCellValue("Salary");
 
+        //save the changes:
+        //open to write to the file: FileInputStream -> reading
+        //                           FileOutputStream -> writing
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        workbook.write(outputStream);
+        outputStream.close();
+        workbook.close();
+        in.close();
 
     }
 }
