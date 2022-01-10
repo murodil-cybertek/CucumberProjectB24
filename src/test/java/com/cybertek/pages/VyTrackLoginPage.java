87185e6ac9f1c760c34faf3d758d6cc1718b602c
Murodil <murodil@cybertekschool.com>
2022-01-10 16:54:38 -0500

    VyTrackLogin DDT test

--- src/test/java/com/cybertek/pages/VyTrackLoginPage.java
+++ src/test/java/com/cybertek/pages/VyTrackLoginPage.java
@@ -4,10 +4,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class VyTrackLoginPage extends BasePage {
+    //no need for @FindBy, since "name" in html is matching variable name
     public WebElement _username;
     public WebElement _password;
 
     @FindBy(id = "_submit")
     public WebElement loginBtn;
+
+    public void login(String userName, String password) {
+        _username.sendKeys(userName);
+        _password.sendKeys(password);
+        loginBtn.click();
+    }
 
 }
