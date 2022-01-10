35d2085fca27e80763605a374c1f31403101b863
Murodil <murodil@cybertekschool.com>
2022-01-10 18:43:05 -0500

    VyTrackLogin DDT test

--- src/test/java/com/cybertek/tests/VyTrackLoginDDTTest.java
+++ src/test/java/com/cybertek/tests/VyTrackLoginDDTTest.java
@@ -4,10 +4,17 @@ import com.cybertek.pages.VyTrackDashboardPage;
 import com.cybertek.pages.VyTrackLoginPage;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class VyTrackLoginDDTTest {
     @Before
@@ -22,30 +29,57 @@ public class VyTrackLoginDDTTest {
     }
 
     @Test
-    public void loginDDTTest() {
+    public void loginDDTTest() throws Exception {
+
+        //open excel workbook:
+        String filePath = "VyTrackQa2Users.xlsx";
+        FileInputStream in = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(in);
+
+        XSSFSheet workSheet = workbook.getSheetAt(0);
+        /**
        String userName = "user1";
        String password = "UserUser123";
        String firstName = "John";
        String lastName = "Doe";
+        */
+        for(int i = 1; i <= workSheet.getLastRowNum(); i++) {
 
-       VyTrackLoginPage loginPage = new VyTrackLoginPage();
-       loginPage.login(userName, password);
+            String userName = workSheet.getRow(i).getCell(0).toString();
+            String password = workSheet.getRow(i).getCell(1).toString();
+            String firstName = workSheet.getRow(i).getCell(2).toString();
+            String lastName = workSheet.getRow(i).getCell(3).toString();
 
-       VyTrackDashboardPage dashboardPage = new VyTrackDashboardPage();
-       System.out.println("Full name = " + dashboardPage.fullName.getText());
+            VyTrackLoginPage loginPage = new VyTrackLoginPage();
+            loginPage.login(userName, password);
 
-       String actualFullName = dashboardPage.fullName.getText();
+            VyTrackDashboardPage dashboardPage = new VyTrackDashboardPage();
+            System.out.println("Full name = " + dashboardPage.fullName.getText());
 
-       // Assert.assertTrue(actualFullName.contains(firstName) && actualFullName.contains(lastName));
+            String actualFullName = dashboardPage.fullName.getText();
 
-       if(actualFullName.contains(firstName) && actualFullName.contains(lastName)) {
-           System.out.println("PASS");
-       } else {
-           System.out.println("FAIL");
-       }
+            // Assert.assertTrue(actualFullName.contains(firstName) && actualFullName.contains(lastName));
 
-        dashboardPage.logout();
+            XSSFCell resultCell = workSheet.getRow(i).getCell(4);
 
+            if (actualFullName.contains(firstName) && actualFullName.contains(lastName)) {
+                System.out.println("PASS");
+                resultCell.setCellValue("PASS");
+            } else {
+                System.out.println("FAIL");
+                resultCell.setCellValue("FAIL");
+            }
+
+            dashboardPage.logout();
+        }
+
+        //save changes in the excel file
+        FileOutputStream out = new FileOutputStream(filePath);
+        workbook.write(out);
+
+        in.close();
+        out.close();
+        workbook.close();
 
     }
 
