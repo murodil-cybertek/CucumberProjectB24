5866bd1a9fbfa5e3588fe675ee637df01e6616df
Murodil <murodil@cybertekschool.com>
2022-01-11 16:29:40 -0500

    Mockaroo Automation Practice

---
+++ src/test/java/com/cybertek/step_definitions/MockarooStepDefs.java
@@ -0,0 +1,39 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+public class MockarooStepDefs {
+    @Given("User is on mockaroo homepage")
+    public void user_is_on_mockaroo_homepage() {
+
+    }
+
+    @Given("Number of rows is {int}")
+    public void number_of_rows_is(Integer int1) {
+
+    }
+
+    @Given("Format is Excel")
+    public void format_is_excel() {
+
+    }
+
+    @When("User clicks on preview")
+    public void user_clicks_on_preview() {
+
+    }
+
+    @Then("following columns should be displayed:")
+    public void following_columns_should_be_displayed(List<String> expectedColumns) {
+
+    }
+
+    @Then("{int} rows of data should be displayed")
+    public void rows_of_data_should_be_displayed(Integer int1) {
+
+    }
+}
