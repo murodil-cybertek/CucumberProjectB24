8cc49f6aa9e589be152e98aca12588fc2e442a6a
Murodil <murodil@cybertekschool.com>
2022-01-25 17:57:50 -0500

    spartan practice

---
+++ src/test/java/com/cybertek/step_definitions/SpartanStepDefs.java
@@ -0,0 +1,40 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.Map;
+
+public class SpartanStepDefs {
+
+    @Given("User is on spartan home page")
+    public void user_is_on_spartan_home_page() {
+
+    }
+
+    @When("User goes to Web Data page")
+    public void user_goes_to_web_data_page() {
+
+    }
+
+    @When("clicks on add spartan")
+    public void clicks_on_add_spartan() {
+
+    }
+
+    @When("enters following data and submits:")
+    public void enters_following_data_and_submits(Map<String, String> spartanInfo) {
+
+    }
+
+    @Then("success message should be displayed")
+    public void success_message_should_be_displayed() {
+
+    }
+
+    @Then("data on confirmation page must be same")
+    public void data_on_confirmation_page_must_be_same() {
+
+    }
+}
